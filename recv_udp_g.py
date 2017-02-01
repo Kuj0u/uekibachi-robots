9df09dd58022b1b6db6954c71c5d570c39438d77
github-daruma <3bda2214@mail.u-tokai.ac.jp>
2017-02-01 16:01:11 +0900

    とりま

--- recv_udp_g.py
+++ recv_udp_g.py
@@ -5,7 +5,7 @@ import socket
 from sys import exit
 from contextlib import closing
 
-host = '192.168.0.0'  #受け取り側のIPアドレス
+host = '127.0.0.1'  #受け取り側のIPアドレス
 port = 4000   #適当なポート番号（このままでも大丈夫）
 bufsize = 512    #バッファサイズ（お互い同じサイズで）
 
@@ -13,8 +13,8 @@ def uketori():
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   with closing(sock):
     sock.bind((host, port))
-    list= sock.recv(bufsize).split(",")
-    recv_g = float(list[0])
+    list= sock.recv(bufsize).split("\t")
+    recv_g = list[1]
     #print recv_g
   return recv_g
 
---
+++ serial_to_udp.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import serial
+import time
+import socket
+
+host = '127.0.0.1'
+port = 4000
+
+def okuru(mozi) :
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(mozi,(host, port))
+    print mozi
+    #time.sleep(0.01)
+
+try :
+    while(1):
+        ser = serial.Serial('/dev/ttyACM0', 9600)
+        moziretu = ser.readline()
+        #print moziretu
+        okuru(moziretu)
+
+finally :
+    ser.close()

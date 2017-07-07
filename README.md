d64a6e857d0e93adeadb527b453e5e0a8b4faaf5
gh-rebanira <rebanira9+git@gmail.com>
2017-07-07 14:45:44 +0900

    実装してるプログラムはこっち
    よろしくぅ！

--- README.md
+++
@@ -1,2 +0,0 @@
-# uekibachi-robots
-This is the software of the robot that are made with graduation research university.
--- serialusb.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import serial
-import time
-
-while(1):
-    ser = serial.Serial('/dev/ttyACM0', 9600)
-    #time.sleep(1)
-    print(ser.readline())
-ser.close()

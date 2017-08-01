5ce66800f675743c528592fbb0b9870e059bf5c9
pipipipi <4bda1208@mail.tokai-u.jp>
2017-08-01 17:18:40 +0900

    左右同時検出できた！

---
+++ python_program/recive1.py
@@ -0,0 +1,41 @@
+import serial
+import time
+
+s = serial.Serial('/dev/ttyACM0',115200, timeout = 10000)
+dataL = 0
+dataR = 0
+A = 0
+B = 0
+oldA = 0
+oldB = 0
+addL = 0
+addR = 0
+countL = 0
+countR = 0
+
+while 1 :
+  data = int(s.readline())
+  addL = (3 & data) - 2
+  addR = (3 & (data / 100)) - 2
+  countL = countL + addL
+  countR = countR + addR
+#  if(data != 0) :
+  print("countL = %5d, countR = %5d, addL = %2d, addR = %2d, data = %4s" % (countL, countR, addL, addR, data))
+
+#  if(oldA == oldB) :
+#    if(oldA != A) :
+#      add = 1
+#    else :
+#      add = -1
+#  else :
+#    if(oldA == A) :
+#      add = 1
+#    else :
+#      add = -1
+#
+#  count = count + add
+
+#  print('A='+A+' B='+B+' oldA='+str(oldA)+' oldB='+str(oldB)+' add='+str(add)+' count='+str(count))
+
+#  oldA = A
+#  oldB = B
